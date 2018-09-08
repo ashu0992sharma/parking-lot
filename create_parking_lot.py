c2d3749843202951a1df4bda4ff49b2df57a568a
ashutosh <ashutosh.sharma@kuliza.com>
2018-09-08 13:36:36 +0530

    parking lot create class

---
+++ create_parking_lot.py
@@ -0,0 +1,19 @@
+import abc
+
+
+class ParkingLot:
+    """Parking Lot class to create a Parking lot"""
+    name = 'create_parking_lot'
+    slots_availability = []
+    slots = []
+
+    def __init__(self, args):
+        """"""
+        quantity = int(args[1])
+        self.quantity = quantity
+        self.slots_availability = [True] * quantity
+        self.slots = [{}] * quantity
+
+    @abc.abstractmethod
+    def execute(self):
+        pass
