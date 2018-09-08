3f17fce1ff4ba05cb7d5f8d91ef56457a85f2358
ashutosh <ashutosh.sharma@kuliza.com>
2018-09-08 18:51:54 +0530

    status command implementation

---
+++ status.py
@@ -0,0 +1,20 @@
+from abs_parking import AbsParking
+
+
+class Status(AbsParking):
+    """class to leave a particular parking slot"""
+    name = "status"
+
+    def __init__(self, args):
+        """
+        constructor to print the status of parking lot
+        :param args: slot number to leave
+        """
+        pass
+
+    def execute(self, parking_lot=None):
+        """executing leave command"""
+        print("Slot No. Registration No Colour")
+        for index, slot in enumerate(parking_lot.slots):
+            if slot:
+                print(f"{index+1}   {slot['registration_no']}   {slot['color']}")
