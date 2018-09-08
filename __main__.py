6f340a6e636a987a5b03c602a290306bf4314f90
ashutosh <ashutosh.sharma@kuliza.com>
2018-09-08 19:18:55 +0530

    slot_numbers_for_cars_with_colour command implemented

--- __main__.py
+++ __main__.py
@@ -3,11 +3,12 @@ from park import Park
 from leave import Leave
 from status import Status
 from slot_number_for_registration_number import SlotRegistrationNo
+from slot_numbers_for_cars_with_colour import SlotCarColor
 from no_command import NoCommand
 
 
 def get_commands():
-    commands = (Park, Leave, Status, SlotRegistrationNo)
+    commands = (ParkingLot, Park, Leave, Status, SlotRegistrationNo, SlotCarColor)
     return {cls.name: cls for cls in commands}
 
 
@@ -18,12 +19,14 @@ def parse_command(commands, args):
 
 commands = get_commands()
 
-args = input()
-# Find and execute the command
-parking_lot = ParkingLot(args.split())
-
 while True:
     args = input()
     # Find and execute the command
-    command = parse_command(commands, args.split())
-    command.execute(parking_lot)
+    parking_lot = parse_command(commands, args.split())
+    parking_lot.execute()
+
+    while True:
+        args = input()
+        # Find and execute the command
+        command = parse_command(commands, args.split())
+        command.execute(parking_lot)
--- slot_numbers_for_cars_with_colour.py
+++ slot_numbers_for_cars_with_colour.py
@@ -1,22 +1,23 @@
 from abs_parking import AbsParking
 
 
-class SlotRegistrationNo(AbsParking):
+class SlotCarColor(AbsParking):
     """class to leave a particular parking slot"""
-    name = "slot_number_for_registration_number"
+    name = "slot_numbers_for_cars_with_colour"
 
     def __init__(self, args):
         """
         constructor to print the slot no for a registration no
         :param args: registration number
         """
-        self.registration_no = args[1]
+        self.color = args[1]
 
     def execute(self, parking_lot=None):
         """executing slot_number_for_registration_number command"""
-
+        slots= []
         for index, slot in enumerate(parking_lot.slots):
-            if slot and slot['registration_no'] == self.registration_no:
-                print(index+1)
-                return
-        print("Not found")
+            if slot and slot['color'] == self.color:
+                slots.append(str(index+1))
+
+        print(", ".join(slots))
+
