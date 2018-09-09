bef01f22126aa8b257827ede6a76e5ec163fa8e4
ashutosh <ashutosh.sharma@kuliza.com>
2018-09-09 18:14:25 +0530

    change in tests file to keep in sys path

---
+++ tests/test_create_parking_lot.py
@@ -0,0 +1,40 @@
+import unittest
+
+import sys, os
+sys.path.append(os.path.abspath(os.path.join('..', '..', 'parking_lot')))
+sys.path.append(os.path.abspath(os.path.join('..', '..', 'parking_lot/src')))
+
+from src.create_parking_lot import CreateParkingLot
+
+
+class CreateParkingLotTest(unittest.TestCase):
+    """test cases for creating a parking lot"""
+
+    def setUp(self):
+        """method for setting up the test fixture before exercising tests"""
+        args = "create_parking_lot 6"
+        self.parking_lot1 = CreateParkingLot(args.split())
+        args = "create_parking_lot 7"
+        self.parking_lot2 = CreateParkingLot(args.split())
+        args = "create_parking_lot 3"
+        self.parking_lot3 = CreateParkingLot(args.split())
+
+    def test_create_parking_lot_quantity(self):
+        """test for parking slots quantity"""
+        assert self.parking_lot1.quantity == 6
+        assert self.parking_lot2.quantity == 7
+        assert self.parking_lot3.quantity == 3
+
+    def test_create_parking_lot_slots_availability(self):
+        """test for  slots availability"""
+        assert self.parking_lot1.slots_availability == [True] * 6
+        assert self.parking_lot2.slots_availability == [True] * 7
+        assert self.parking_lot3.slots_availability == [True] * 3
+
+    def test_slots_vehicle_details(self):
+        """test for vehicle details"""
+        assert self.parking_lot1.slots == [{}] * 6
+        assert self.parking_lot2.slots == [{}] * 7
+        assert self.parking_lot3.slots == [{}] * 3
+
+
